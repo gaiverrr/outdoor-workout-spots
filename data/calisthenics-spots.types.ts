579857599283a8bda4a6e7a1ebc291afe743f25e
Yuriy Kaygorodov <gaiverrr@gmail.com>
2025-11-20 19:57:46 +0000

    Add .idea to .gitignore, create settings.local.json, and define CalisthenicsSpot types

---
+++ data/calisthenics-spots.types.ts
@@ -0,0 +1,22 @@
+export interface CalisthenicsSpotDetails {
+  equipment: string[];
+  disciplines: string[];
+  description: string;
+  features: {
+    type: string;
+  };
+  images: string[];
+  rating: number;
+}
+
+export interface CalisthenicsSpot {
+  id: number;
+  title: string;
+  name?: string | null;
+  lat?: number;
+  lon?: number;
+  address?: string;
+  details?: CalisthenicsSpotDetails;
+}
+
+export type CalisthenicsSpots = CalisthenicsSpot[];
