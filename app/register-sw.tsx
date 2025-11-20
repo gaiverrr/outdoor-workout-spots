789cc57c312b1f19b60d75f1243a81a527f884f7
Yuriy Kaygorodov <gaiverrr@gmail.com>
2025-11-20 21:06:04 +0000

    Comment out service worker registration code in RegisterServiceWorker component

--- app/register-sw.tsx
+++ app/register-sw.tsx
@@ -5,14 +5,14 @@ import { useEffect } from "react";
 export function RegisterServiceWorker() {
   useEffect(() => {
     if ("serviceWorker" in navigator) {
-      navigator.serviceWorker
-        .register("/sw.js")
-        .then((registration) => {
-          console.log("Service Worker registered:", registration);
-        })
-        .catch((error) => {
-          console.error("Service Worker registration failed:", error);
-        });
+      // navigator.serviceWorker
+      //   .register("/sw.js")
+      //   .then((registration) => {
+      //     console.log("Service Worker registered:", registration);
+      //   })
+      //   .catch((error) => {
+      //     console.error("Service Worker registration failed:", error);
+      //   });
     }
   }, []);
 
